587d70e35cd6a2f6123700cac36faabba310994b
Julian Enochs <julianenochs@yahoo.com>
2019-10-23 07:56:37 -0600

    Ypdate handleChange method in LoginForm

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -14,8 +14,8 @@ class LoginForm extends Component {
 
     }
 
-    handleChange = () => {
-
+    handleChange = (e) => {
+        this.setState({ [e.target.name] : e.target.value })
     }
 
     render() {
