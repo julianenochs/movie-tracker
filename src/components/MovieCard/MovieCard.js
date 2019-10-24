26771fc48f783f7677fb54fd44119dd4482fd3ce
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-10-24 16:14:51 -0600

    Add favorite button to each movie
    Co-authored-by: Brady Bridges <bradyjbridges@gmail.com>

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './MovieCard.scss';
+import star from '../../images/star-white.svg';
+import starFavorited from '../../images/star-favorited.svg';
 
 const MovieCard = ({ title, poster, overview }) => {
-
   return (
     <section className='movie__card'>
-      <h1 className='movie__title'>{title}</h1>
-      <img src={`https://image.tmdb.org/t/p/original/${poster}`} alt='movie poster' />
-      <p className='overview'>
-        {overview}
-      </p>
+      <div>
+        <h1 className='movie__title'>{title}</h1>
+        <img src={star} />
+      </div>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${poster}`}
+        alt='movie poster'
+      />
+      <p className='overview'>{overview}</p>
       {/* <img src='https://www.pinclipart.com/picdir/middle/33-337369_heart-shaped-clipart-instagram-heart-sign-icon-transparent.png' 
           className='favorite__logo favorited'
       /> */}
