c941fe1ebd268a676bec90fbac0e29201d949917
Julian Enochs <julianenochs@yahoo.com>
2019-10-23 19:05:14 -0600

    Update container styling

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './header.scss';
 
 const Header = () => {
-
+  return(
+    <header className='header'>
+      <h1>NEWFLIX</h1>
+      <button>Sign In</button>
+    </header>
+  )
 }
 
 export default Header;
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,9 +7,12 @@ const MovieCard = ({ title, poster, overview }) => {
     <section className='movie__card'>
       <h1>{title}</h1>
       <img src={`https://image.tmdb.org/t/p/original/${poster}`} alt='movie poster' />
-      <p>
+      <p className='overview'>
         {overview}
       </p>
+      <img src='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX17743925.jpg' 
+          className='favorite__logo favorited'
+      />
     </section>
   );
 };
